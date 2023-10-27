1f4fcc528a8c332a61a5d17c6d944a9eceb9a91e
kaseen <srbtrxtheinsane@gmail.com>
2023-10-27 17:48:13 +0200

    lib.rs: get_round_info return date instead of unix timestamp

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,26 @@ use ethers::{
     utils::format_units,
     prelude::abigen
 };
+use chrono::{
+    prelude::DateTime,
+    Utc, Local
+};
 
 abigen!(LuckySix, "abi.json");
 
+/*
+ *  TODO: Config
+ *  - Provider
+ *  - LuckySix adress
+ *  - Set time zone, ex: get_round_info()
+ */
+
 async fn get_contract_instance() -> Result<LuckySix<Provider<Http>>, Box<dyn std::error::Error>> {
-    // TODO: Provider to config
     let provider_url = std::env::var("HTTP_SEPOLIA").expect("HTTP_SEPOLIA environment variable not found");
-
-    // TODO: Address to config
     let lucky_six_address = "0x5d65cff1f21fcfedd194ef7e15e66ae31ab6dcb7";
-
     let provider = Provider::<Http>::try_from(provider_url)?; 
     let address: Address = lucky_six_address.parse()?;
     let contract = LuckySix::new(address, provider.into());
-
     Ok(contract)
 }
 
@@ -33,10 +39,18 @@ fn parse_to_denomination(input: U256, to_denomnination: &str) -> String {
 }
 
 #[tokio::main]
-pub async fn get_round_info() -> Result<(U256, U256, bool), Box<dyn std::error::Error>> {
+pub async fn get_round_info() -> Result<(U256, String, bool), Box<dyn std::error::Error>> {
     let contract = get_contract_instance().await?;
     let round_info = contract.round_info().call().await?;
-    Ok(round_info)
+
+    // Convert to Local timezone
+    let datetime = DateTime::<Utc>::from_timestamp(
+        round_info.1.as_u64().try_into().unwrap(), 0
+    ).expect("Invalid timestamp").with_timezone(&Local);
+
+    let timestamp_string = datetime.format("%d/%m/%Y %H:%M:%S").to_string();
+
+    Ok((round_info.0, timestamp_string, round_info.2))
 }
 
 #[tokio::main]
