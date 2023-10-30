b558e2367c237b8388b8a8708a63d511ddc121e7
kaseen <srbtrxtheinsane@gmail.com>
2023-10-30 17:49:42 +0100

    lib.rs: parse_to_denomination returns Result<>

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
+use chrono::{prelude::DateTime, Local, Utc};
 use ethers::{
-    providers::{ Http, Provider },
-    signers::{ LocalWallet, Signer },
-    types::{ Address, U256 },
+    utils::{ ConversionError, format_units },
+    signers::{LocalWallet, Signer},
+    providers::{Http, Provider},
+    types::{Address, U256},
     middleware::SignerMiddleware,
-    utils::format_units,
     core::types::Chain,
-    prelude::abigen
-};
-use chrono::{
-    prelude::DateTime,
-    Utc, Local
+    prelude::abigen,
 };
 
 type BoxError = Box<dyn std::error::Error>;
@@ -24,15 +21,19 @@ abigen!(LuckySix, "abi.json");
  */
 
 async fn get_provider() -> Result<Provider<Http>, BoxError> {
-    let provider_url = std::env::var("HTTP_SEPOLIA").expect("HTTP_SEPOLIA environment variable not found");
+    let provider_url =
+        std::env::var("HTTP_SEPOLIA").expect("HTTP_SEPOLIA environment variable not found");
     let provider = Provider::<Http>::try_from(provider_url)?;
 
     Ok(provider)
 }
 
 async fn get_wallet() -> Result<LocalWallet, BoxError> {
-    let private_key = std::env::var("PRIVATE_KEY").expect("PRIVATE_KEY environment variable not found");
-    let wallet = private_key.parse::<LocalWallet>()?.with_chain_id(Chain::Sepolia);
+    let private_key =
+        std::env::var("PRIVATE_KEY").expect("PRIVATE_KEY environment variable not found");
+    let wallet = private_key
+        .parse::<LocalWallet>()?
+        .with_chain_id(Chain::Sepolia);
 
     Ok(wallet)
 }
@@ -50,20 +51,19 @@ async fn get_contract_instance() -> Result<LuckySix<Client>, BoxError> {
 
     let lucky_six_address = "0x5d65cff1f21fcfedd194ef7e15e66ae31ab6dcb7";
     let address: Address = lucky_six_address.parse()?;
-    let contract = LuckySix::new(address, client.into()); 
+    let contract = LuckySix::new(address, client.into());
 
     Ok(contract)
 }
 
-fn parse_to_denomination(input: U256, to_denomnination: &str) -> String {
+fn parse_to_denomination(input: U256, to_denomnination: &str) -> Result<String, ConversionError> {
     let parsed_result = match to_denomnination {
-        "eth" => format_units(input, "Ether"),
+        "eth" => format_units(input, "Ether"), 
         "gwei" => format_units(input, "Gwei"),
         "wei" => format_units(input, "Wei"),
-        // TODO
-        _ => Ok(String::from("Error"))
+        _ => Err(ConversionError::UnrecognizedUnits(to_denomnination.to_string()))
     };
-    String::from(parsed_result.unwrap().trim_end_matches('0').trim_end_matches('.'))
+    parsed_result
 }
 
 #[tokio::main]
@@ -72,9 +72,9 @@ pub async fn get_round_info() -> Result<(U256, String, bool), BoxError> {
     let round_info = contract.round_info().call().await?;
 
     // Convert to Local timezone
-    let datetime = DateTime::<Utc>::from_timestamp(
-        round_info.1.as_u64().try_into().unwrap(), 0
-    ).expect("Invalid timestamp").with_timezone(&Local);
+    let datetime = DateTime::<Utc>::from_timestamp(round_info.1.as_u64().try_into().unwrap(), 0)
+        .expect("Invalid timestamp")
+        .with_timezone(&Local);
 
     let timestamp_string = datetime.format("%d/%m/%Y %H:%M:%S").to_string();
 
@@ -86,7 +86,12 @@ pub async fn get_platform_fee() -> Result<String, BoxError> {
     let contract = get_contract_instance().await?;
     let platform_fee_wei = contract.platform_fee().call().await?;
     let unit = "eth";
-    let platform_fee_eth = format!("{} {}", parse_to_denomination(platform_fee_wei, unit), unit);
+
+    let to_denomination = parse_to_denomination(platform_fee_wei, unit).unwrap();
+    let parsed_result = to_denomination.trim_end_matches('0').trim_end_matches('0');
+    
+    let platform_fee_eth = format!("{} {}", parsed_result, unit);
+
     Ok(platform_fee_eth)
 }
 
@@ -97,11 +102,9 @@ pub async fn print_drawn_numbers_for_round(n: U256) -> Result<(), BoxError> {
 
     // TODO: Let it public on blockchain
     let bonus_multiplier = [
-        0, 0, 0, 0, 0, 10000, 7500, 5000, 
-        2500, 1000, 500, 300, 200, 150, 100, 
-        90, 80, 70, 60, 50, 40, 30, 25, 20, 
-        15, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
-    ]; 
+        0, 0, 0, 0, 0, 10000, 7500, 5000, 2500, 1000, 500, 300, 200, 150, 100, 90, 80, 70, 60, 50,
+        40, 30, 25, 20, 15, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
+    ];
 
     for i in 0..5 {
         print!("{} ", result[i]);
@@ -109,12 +112,12 @@ pub async fn print_drawn_numbers_for_round(n: U256) -> Result<(), BoxError> {
     println!();
     for i in 5..15 {
         let left_mul = bonus_multiplier[i];
-        let middle_mul = bonus_multiplier[i+10];
-        let right_mul = bonus_multiplier[i+20];
+        let middle_mul = bonus_multiplier[i + 10];
+        let right_mul = bonus_multiplier[i + 20];
 
         let left_num = result[i];
-        let middle_num = result[i+10];
-        let right_num = result[i+20];
+        let middle_num = result[i + 10];
+        let right_num = result[i + 20];
 
         print!("{left_mul:>5}: {left_num:>2}     ");
         print!("{middle_mul:>2}: {middle_num:>2}     ");
@@ -134,4 +137,3 @@ pub async fn get_tickets_for_round(n: U256) -> Result<(), BoxError> {
 
     Ok(())
 }
-
